8418b2c50efcefc3176fe1f45ad1cde7ad5ceb2e
luiscruz0555 <luis803@revature.net>
2022-08-07 18:04:00 -0600

    Just missing update user func

--- AlchemistRoom/src/main/java/com/revature/controllers/UserController.java
+++ AlchemistRoom/src/main/java/com/revature/controllers/UserController.java
@@ -3,8 +3,13 @@ package com.revature.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +37,8 @@ public class UserController {
 	
 	
 		
+	//This method will be used to login a user.
+	//path is: POST - http://localhost:5555/data/user/login
 	@PostMapping(value="/login")
 	public ResponseEntity<AlchemistUser> login(@RequestBody LoginDTO lDTO) {
 		
@@ -53,6 +60,52 @@ public class UserController {
 		
 		
 	}
+	
+	//This method will be used to add a new user. isadmin will have to be set to null on Angular side w/ JSON object
+	//path is: POST - http://localhost:5555/data/user
+	@PostMapping
+	public ResponseEntity addUser(@RequestBody AlchemistUser user) {
+		
+		AlchemistUser newUser = uDAO.save(user);
+		
+		if(newUser == null ) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+		return ResponseEntity.accepted().body(newUser);
+	}
+	
+	//this method will be used to find a user by ID
+	//path is GET - http://localhost:5555/data/user/byId/{id}
+	@GetMapping(value =("/byId/{id}"))
+	public ResponseEntity<AlchemistUser> findById(@PathVariable int id){
+		
+		Optional<AlchemistUser> userOptional = uDAO.findById(id);
+		
+		if(userOptional.isPresent()) {
+			AlchemistUser user = userOptional.get();
+			return ResponseEntity.ok(user);
+		}
+		
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	//this method will be used to delete a user by their id in the path
+	//path is DELETE - http://localhost:5555/data/user/deleteById/{id}
+	@DeleteMapping(value=("deleteById/{id}"))
+	public ResponseEntity<?> deleteById(@PathVariable int id){
+		
+		try{uDAO.deleteById(id);
+		
+		return new ResponseEntity<String>("User deleted: ", HttpStatus.OK);
+		} catch(EmptyResultDataAccessException e) {
+			return new ResponseEntity<String>("User not found", HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	
 	
 
 }
