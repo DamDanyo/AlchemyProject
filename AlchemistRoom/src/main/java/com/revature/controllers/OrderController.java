8db3c3ef358235b5239e53d2aa92786953685d1a
genesisvlzqz <genesisvlzqz@gmail.com>
2022-08-10 11:13:52 -0400

    Insert Order

--- AlchemistRoom/src/main/java/com/revature/controllers/OrderController.java
+++ AlchemistRoom/src/main/java/com/revature/controllers/OrderController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,6 +50,18 @@ public class OrderController {
 		}
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	//Insert Order 
+	@PostMapping
+	public ResponseEntity<Order> addOrder(@RequestBody Order o){
+		
+		Order newOrder = oDAO.save(o);
+		if(newOrder == null) {
+			return ResponseEntity.badRequest().build();
+			
+		}
+		return ResponseEntity.accepted().body(newOrder);
 	}
 	
 	//Backend Methods-----------------------------------	
