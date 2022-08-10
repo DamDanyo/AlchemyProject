353c1e4fb423e21e2607619b0da4e3c490e2cc18
genesisvlzqz <genesisvlzqz@gmail.com>
2022-08-10 10:51:51 -0400

    Order getAll/getByID

--- AlchemistRoom/src/main/java/com/revature/controllers/OrderController.java
+++ AlchemistRoom/src/main/java/com/revature/controllers/OrderController.java
@@ -1,11 +1,18 @@
 package com.revature.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.daos.OrderDAO;
+import com.revature.models.Order;
 
 @CrossOrigin
 @RestController
@@ -14,14 +21,35 @@ public class OrderController {
 	
 	private OrderDAO oDAO;
 	@Autowired
-	public OrderController() {
+	public OrderController(OrderDAO oDAO) {
 		super();
+		this.oDAO = oDAO;
 		// TODO Auto-generated constructor stub
 	}
 	//MVC Methods to Handle HTTP Requests==================
 	
 	
 	//Frontend Methods-----------------------------------
+	//Get all potions
+	@GetMapping(value="/all")
+	public ResponseEntity<List<Order>> getAllOrder(){
+		return ResponseEntity.ok(oDAO.findAll());
+	}
+	
+	//Get Potion by id
+	@GetMapping(value="/byId/{id}")
+	public ResponseEntity<Order> findById(@PathVariable int id){
+		
+		Optional<Order> optionalOrder = oDAO.findById(id);
+		
+		if (optionalOrder.isPresent()) {
+			Order o = optionalOrder.get();
+			return ResponseEntity.ok(o);
+		}
+		
+		return ResponseEntity.noContent().build();
+	}
+	
 	//Backend Methods-----------------------------------	
 	
 
