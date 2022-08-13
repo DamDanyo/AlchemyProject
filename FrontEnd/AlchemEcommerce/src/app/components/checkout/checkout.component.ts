00450a58e89c37ccb707a150fbfc015a66bafb27
MatthewGiordano <mattgiordano2@gmail.com>
2022-08-13 16:34:08 -0400

    get data testing

--- FrontEnd/AlchemEcommerce/src/app/components/checkout/checkout.component.ts
+++ FrontEnd/AlchemEcommerce/src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PotionsService } from 'src/app/services/potions.service';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ps:PotionsService) {}
+
+  getPotions() {
+  this.ps.getPotionTest().subscribe(
+         {next:(data)=>{
+           console.log(data)
+         }, error:(err)=>{
+           console.log(err)
+         }
+       } )
+     
+      }
 
   ngOnInit(): void {
+
+    this.ps.getPotionTest().subscribe(
+         {next:(data)=>{
+           console.log(data)
+         }, error:(err)=>{
+           console.log(err)
+         }
+       } )
+
   }
 
 }
--- FrontEnd/AlchemEcommerce/src/app/services/order.service.ts
+++ FrontEnd/AlchemEcommerce/src/app/services/order.service.ts
@@ -1,36 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Order } from '../models/order';
+import { UserCreds } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  order:Order = {
+  //order:Order = {
     // userid: 11,
     // username: "daniel",
     // password: "password",
     // isadmin: true,
     // orderaddress: "high st",
-    ordertotal: ,   
-    items: "",
-    itemsquantity: "",
+   //ordertotal: ,   
+    //items: "",
+   // itemsquantity: "",
     
-  }
+ // }
 
 
 
   
-  constructor(private http: HttpClient) {}
-  private loginUrl = 'http://localhost:5555/data/user/login';
-  private addUrl = 'http://localhost:5555/data/user';
+  //constructor(private http: HttpClient) {}
+  //private loginUrl = 'http://localhost:5555/data/user/login';
+  //private addUrl = 'http://localhost:5555/data/user';
 
-  login(userCreds: UserCreds): Observable<UserCreds> {
-    return this.http.post<UserCreds>(
-      this.loginUrl,
-      userCreds
-    ) as Observable<UserCreds>;
-  }
+  //login(userCreds: UserCreds): Observable<UserCreds> {
+    //return this.http.post<UserCreds>(
+      //this.loginUrl,
+      //userCreds
+   // ) as Observable<UserCreds>;
+ //}
 
 
 
