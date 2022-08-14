a1a83754b38452ad70f5a96ffda99e31eab031e1
DamDanyo <dmitch1423@gmail.com>
2022-08-14 18:34:37 -0400

    cart works

--- FrontEnd/AlchemEcommerce/src/app/components/checkout/checkout.component.ts
+++ FrontEnd/AlchemEcommerce/src/app/components/checkout/checkout.component.ts
@@ -17,9 +17,10 @@ export class CheckoutComponent implements OnInit {
     for (let i = 0; i < this.cart.length; i++) {
       let potion = this.cart[i].potion;
       let q = this.cart[i].potionQuantity;
-      let q2 = this.cart[i].potion.potionvalue
-      console.log(q)
-      this.Total = q2 * q;
+      let q2 = this.cart[i].potion.potionvalue;
+      let q3: number = q * q2;
+      console.log(q);
+      this.Total += q3;
     }
   }
 
@@ -27,22 +28,25 @@ export class CheckoutComponent implements OnInit {
     let localCart = localStorage.getItem('cart');
     if (localCart != null) {
       let cartArray = JSON.parse(localCart);
-      console.log(index)
-      let q = cartArray.potionQuantity
-      this.Total = cartArray[index].potion.potionvalue * q
-      cartArray.splice(index, 1)
-      localStorage.setItem("cart", JSON.stringify(cartArray))
-      this.cart = cartArray
-       if (this.cart.length == 0){
-        this.Total = 0
-       }
-    
-    } 
+      console.log(index);
+      let q: number = cartArray[index].potionQuantity;
+      let q2: number = cartArray[index].potion.potionvalue;
+      let q3: number = q * q2;
+      console.log(q);
+      console.log(q2);
+      this.Total = this.Total - q3;
+      console.log(this.Total);
+      cartArray.splice(index, 1);
+      localStorage.setItem('cart', JSON.stringify(cartArray));
+      this.cart = cartArray;
+      if (this.cart.length == 0) {
+        this.Total = 0;
+      }
+    }
 
-    
     console.log(this.cart);
   }
- 
+
   cart: Array<any> = [];
   Total: number = 0;
   constructor() {}
